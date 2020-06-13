5439649623d64b0d8a4f0d587af1d57cf83dbd4a
Pedro Felipe Dominguite <pfdominguite@gmail.com>
2020-06-12 23:40:19 -0300

    Changed to 'https'

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://broker.shiftr.io',
+  baseURL: 'https://broker.shiftr.io',
   auth: { username: '09f3b188', password: '879abfcbf91337ee' },
 });
 
