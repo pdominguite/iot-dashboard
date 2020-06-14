92e441e356ca8f3403656e388adf0e3d2b50947a
Pedro Felipe Dominguite <pfdominguite@gmail.com>
2020-06-13 23:02:44 -0300

    Fixed environment variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const username = process.env.BROKER_USERNAME ? process.env.BROKER_USERNAME : '';
-const password = process.env.BROKER_PASSWORD ? process.env.BROKER_PASSWORD : '';
+const username = process.env.REACT_APP_BROKER_USERNAME
+  ? process.env.REACT_APP_BROKER_USERNAME
+  : '';
+const password = process.env.REACT_APP_BROKER_USERNAME
+  ? process.env.REACT_APP_BROKER_USERNAME
+  : '';
 
 const api = axios.create({
   baseURL: 'https://broker.shiftr.io',
